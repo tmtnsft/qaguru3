e317579a1de2f2e6513dd21fffc367de82211a7b
dariae <d.egorova89@gmail.com>
2022-01-25 01:25:42 +0300

    version1

--- src/test/java/guru/qa3/CodeSampleCheck.java
+++ src/test/java/guru/qa3/CodeSampleCheck.java
@@ -2,8 +2,8 @@ package guru.qa3;
 
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
 
 public class CodeSampleCheck {
 
@@ -12,8 +12,11 @@ public class CodeSampleCheck {
         //Откройте страницу Selenide в Github
         open("https://github.com/selenide/selenide");
         //Перейдите в раздел Wiki проекта
-        $("wiki-tab").click();
+        $("[data-tab-item=i5wiki-tab]").click();
         //Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
+        $("#wiki-body").shouldHave(text("Soft assertions"));
         //Откройте страницу SoftAssertions, проверьте что внутри есть пример кода для JUnit5
+
+        sleep(5000);
     }
 }
