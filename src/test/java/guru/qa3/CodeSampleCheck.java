c8dfceaa082809b6575f3aaed7187c26757a8b12
dariae <d.egorova89@gmail.com>
2022-01-25 03:13:16 +0300

    version2 open github page via list

--- src/test/java/guru/qa3/CodeSampleCheck.java
+++ src/test/java/guru/qa3/CodeSampleCheck.java
@@ -1,5 +1,7 @@
 package guru.qa3;
 
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
@@ -7,10 +9,17 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class CodeSampleCheck {
 
+    @BeforeAll
+    static void BrowserSize() {
+        Configuration.browserSize = "1920x3072";
+    }
+
     @Test
     void softAssertionsContainsCodeSample () {
         //Откройте страницу Selenide в Github
-        open("https://github.com/selenide/selenide");
+        open("https://github.com/");
+        $("[data-test-selector=nav-search-input]").setValue("selenide").pressEnter();
+        $$("ul.repo-list li").first().$("a").click();
         //Перейдите в раздел Wiki проекта
         $("[data-tab-item=i5wiki-tab]").click();
         //Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
