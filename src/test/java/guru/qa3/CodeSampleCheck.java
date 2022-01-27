294b88df5c8f7119ca50573cc2ab02e8e09cd4a7
dariae <d.egorova89@gmail.com>
2022-01-28 01:23:40 +0300

    CodeSampleCheck done

--- src/test/java/guru/qa3/CodeSampleCheck.java
+++ src/test/java/guru/qa3/CodeSampleCheck.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class CodeSampleCheck {
@@ -23,9 +24,10 @@ public class CodeSampleCheck {
         //Перейдите в раздел Wiki проекта
         $("[data-tab-item=i5wiki-tab]").click();
         //Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
-        $("#wiki-body").shouldHave(text("Soft assertions"));
+        $(".filter-bar").$(byText("SoftAssertions"));
         //Откройте страницу SoftAssertions, проверьте что внутри есть пример кода для JUnit5
-
-        sleep(5000);
+        $(".wiki-pages-box").$(byText("Show 2 more pages…")).pressEnter()
+                .closest("div").$(byText("SoftAssertions")).click();
+        $(".markdown-body").shouldHave(text("JUnit5")).find(byText("@Test"));
     }
 }
